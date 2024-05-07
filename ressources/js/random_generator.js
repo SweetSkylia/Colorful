b69c52a0c518bb4cccdf4be91f0553e67a76d0b3
SweetSkylia <146096843+SweetSkylia@users.noreply.github.com>
2024-05-07 13:38:43 +0200

    update design & added contributors code
    changing my code to a more efficient by Lurnok

--- ressources/js/random_generator.js
+++ ressources/js/random_generator.js
@@ -6,7 +6,6 @@ document.addEventListener('keydown', function(event) {
 })
 
 addEventListener("load", (event) => {});
-
 onload = (event) => {
     GenerateColors();
 };
@@ -16,21 +15,13 @@ var columns = document.getElementsByClassName('colorcol');
 
 function GenerateColors(){
     colorArray.length = 0;
-    for (let i = 0; i < 6; i++) {
-        
-        var letters = '0123456789ABCDEF';
-        var color = '#';
-        for (var j = 0; j < 6; j++) {
-          color += letters[Math.floor(Math.random() * 16)];
-        }
-        colorArray.push(color);
-        
-    }
-    // console.log(columns);
-    // console.log(colorArray);
     for (var i = 0; i < columns.length; i++) {
-        // console.log(columns[i] + " | " + colorArray[i])
+        colorArray[i] = 'RGB(' + randHexVal() + ',' + randHexVal() + ',' + randHexVal() + ')';  
         columns[i].style.backgroundColor = colorArray[i];
     }
 }
 
+
+function randHexVal() {
+    return Math.floor(Math.random() * 256);
+}
