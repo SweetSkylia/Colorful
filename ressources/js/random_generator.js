46997715bc6f72d88228fea075622149b64cfe11
SweetSkylia <146096843+SweetSkylia@users.noreply.github.com>
2024-05-07 15:17:18 +0200

    generator fix and spacebar control fix

--- ressources/js/random_generator.js
+++ ressources/js/random_generator.js
@@ -1,21 +1,21 @@
 document.addEventListener('keydown', function(event) {
-    if(event.key === " "){
-        event.preventDefault;
+    if(event.key === " " && event.target === document.body){
+        event.preventDefault();
         GenerateColors();
     } 
 })
 
-addEventListener("load", (event) => {});
+
 onload = (event) => {
     GenerateColors();
 };
 
 const colorArray = [];
-var columns = document.getElementsByClassName('colorcol');
+let columns = document.getElementsByClassName('colorcol');
 
 function GenerateColors(){
     colorArray.length = 0;
-    for (var i = 0; i < columns.length; i++) {
+    for (let i = 0; i < columns.length; i++) {
         colorArray[i] = 'RGB(' + randHexVal() + ',' + randHexVal() + ',' + randHexVal() + ')';  
         columns[i].style.backgroundColor = colorArray[i];
     }
